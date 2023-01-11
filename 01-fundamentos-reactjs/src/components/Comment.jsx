320b63500f911dc9d29bc5d658a2a98a18649f3b
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2023-01-11 10:12:17 -0300

    feat: add comments

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/giovanaraphaelli.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Giovana Raphaelli</strong>
+              <time
+                title="10 de Janeiro as 10h00"
+                dateTime="2022-01-10 10:00:00"
+              >
+                Cerca de 1hr atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
