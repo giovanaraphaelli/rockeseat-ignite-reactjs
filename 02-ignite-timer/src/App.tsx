64dfd84fa0056e1e137bc4b13c48181f2f7a8349
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2023-01-24 09:18:21 -0300

    feat: styled-component exemple

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,3 +1,13 @@
+import { Button } from './components/Button';
+
 export function App() {
-  return <div></div>;
+  return (
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
+  );
 }
