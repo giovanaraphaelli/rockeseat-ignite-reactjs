ca3837ef3e1513a215576438bf13bdcd7fc4958c
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2023-01-27 08:53:22 -0300

    refactor: header styles

--- 02-ignite-timer/src/components/Header/styles.ts
+++ 02-ignite-timer/src/components/Header/styles.ts
@@ -26,6 +26,10 @@ export const HeaderContainer = styled.header`
       &.active {
         color: ${(props) => props.theme['green-500']};
       }
+
+      &:focus {
+        box-shadow: none;
+      }
     }
   }
 `;
