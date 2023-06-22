93c3a565220231a6af8d50bf65f5e7bade800ce7
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2023-06-21 21:56:35 -0300

    feat: create Transactions Page

--- 03-dt-money/src/App.tsx
+++ 03-dt-money/src/App.tsx
@@ -1,11 +1,12 @@
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/theme/default';
+import { Transactions } from './pages/Transactions';
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Hello Word</h1>
+      <Transactions />
     </ThemeProvider>
   );
 }
